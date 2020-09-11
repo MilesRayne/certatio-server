4e367d821698fb8b738eae8c3c07ddd2866243bf
Milos Rajkovic <milosrajkovic98@yahoo.com>
2020-09-11 14:27:31 +0200

    Sredjivanje diskonektovanja

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const server = io.listen(3000);
 
 server.on("connection", function (socket) {
 
+  //pracenje sobe u kojoj je igrac, mozda postoji bolji nacin sa socket.rooms ?
+  let currentRoom = null;
+
   //logovanje konektovanja novog korisnika na server igre
   console.log("user", socket.id, "connected to the game server");
   socket.emit("welcome", "welcome man");
@@ -14,6 +17,7 @@ server.on("connection", function (socket) {
     console.log("creating room", roomID, "for host", socket.id);
 
     socket.join(roomID);
+    currentRoom = roomID;
     socket.emit("room:created", roomID);
   });
 
@@ -22,10 +26,18 @@ server.on("connection", function (socket) {
   socket.on("room:join", (roomID) => {
 
     //bilo bi korisno ubaciti da se korisnik diskonektuje iz svih ostalih soba pri ulasku u novu
-    socket.join(roomID);
 
+    socket.join(roomID);
+    currentRoom = roomID;
+    //posalji nazad korisniku da se joinovao
+    socket.emit("room:joined", roomID);
 
     //socket (korisnik koji ulazi) emituje drugima da je usao u sobu
     socket.to(roomID).emit("room:userJoined", socket.id);
   })
+
+  socket.on("disconnect", () => {
+
+    socket.to(currentRoom).emit("room:userLeft", socket.id);
+  });
 });
