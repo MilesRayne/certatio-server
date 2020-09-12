db4b27487fa0cd669c87add8c776b1e6c2f18f3e
Milos Rajkovic <milosrajkovic98@yahoo.com>
2020-09-12 20:46:36 +0200

    check for words contained within words

--- utils.js
+++ utils.js
@@ -4,10 +4,21 @@ function generateWords(wordCount = 5) {
     const chosenWords = [];
     do {
         let word = words[Math.floor(Math.random() * words.length)];
-        if (!chosenWords.includes(word)) chosenWords.push(word);
+        if (!chosenWords.includes(word) && !isContained(chosenWords, word)) chosenWords.push(word);
     } while (chosenWords.length < wordCount);
     console.log(chosenWords);
     return chosenWords;
+}
+
+function isContained(chosenWords, word) {
+
+    for (let element of chosenWords) {
+        if (element.includes(word) || word.includes(element)) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 module.exports = {
